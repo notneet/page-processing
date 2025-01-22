b8bdd21f6e3ae6df7fc39e7a449dfd470a2082cb
アニップ <hanivan20@gmail.com>
2025-01-22 21:23:10 +0700

    update: configable 2

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
-import { ValidationPipe } from '@nestjs/common';
+import { LogLevel, ValidationPipe } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+async function setupApp(): Promise<[number, LogLevel[]]> {
+  const configModule = await NestFactory.createApplicationContext(ConfigModule);
+  const configService = configModule.get(ConfigService);
+  const port = configService.get<number>('APP_PORT', 3000);
+  const isDev = configService.get<string>('NODE_ENV') === 'development';
+  const logger: LogLevel[] = isDev
+    ? ['log', 'error', 'warn', 'debug']
+    : ['log', 'error', 'warn'];
+  return [port, logger];
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const [port, logger] = await setupApp();
+  const app = await NestFactory.create(AppModule, { logger });
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
